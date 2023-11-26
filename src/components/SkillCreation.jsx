939708f8a0fa5dccaec56783023c31d128ecceac
Peter R <prodger2@gmail.com>
2023-11-26 15:00:36 +0100

    skills added to user object upon creation

--- src/components/SkillCreation.jsx
+++ src/components/SkillCreation.jsx
@@ -5,8 +5,26 @@ import { AuthContext } from "../context/Auth.context";
 
 function SkillCreation() {
   const [title, setTitle] = useState("");
-  const { authenticateUser } = useContext(AuthContext);
+  const { authenticateUser, user, setUser } = useContext(AuthContext);
   const nav = useNavigate();
+
+  const updateUserSkills = async (skillId) => {
+    try {
+      const userId = user._id;
+      console.log("Frontend - UserID:", userId); // Log user ID
+      console.log("Frontend - SkillID:", skillId); // Log skill ID
+      const res = await axios.put(
+        `http://localhost:5005/user/${user._id}/add-skill`,
+        { skillId }
+      );
+
+      console.log("Update User Skills Response:", res.data);
+
+      // Handle the response as needed
+    } catch (error) {
+      console.error("Error updating user skills:", error);
+    }
+  };
 
   const handleSkillCreation = async (e) => {
     e.preventDefault();
@@ -19,6 +37,19 @@ function SkillCreation() {
         }
       );
       console.log("This is the axios post result", res);
+
+      const createdSkill = res.data.skill;
+      const skillId = createdSkill._id;
+      console.log("Frontend - Created SkillID:", skillId); // Log created skill ID
+      // const skillTitle = createdSkill.title;
+
+      // Update the user's skills array
+      setUser((prevUser) => ({
+        ...prevUser,
+        skills: [...prevUser.skills, skillId],
+      }));
+
+      await updateUserSkills(user._id, skillId);
 
       await authenticateUser();
       nav("/profile");
