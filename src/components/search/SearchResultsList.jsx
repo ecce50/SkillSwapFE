656bc4cd5f23befe0a12b63bd8e31dda2e4d9ea0
Mariana Ribeiro de Carvalho <mariana.rcarvalho.992gmail.com>
2024-03-03 12:01:42 +0000

    working on dynamic search logic. g=gardening!=coding

--- src/components/search/SearchResultsList.jsx
+++ src/components/search/SearchResultsList.jsx
@@ -1,24 +1,47 @@
 // SearchResultsList.jsx
-import React from "react";
-import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useLocation, Link } from "react-router-dom";
 import SkillResultItem from "./SkillResultItem";
 import ClassResultItem from "./ClassResultItem";
 
-const SearchResultsList = () => {
+const SearchResultsList = ({onSearch}) => {
   // Use the useLocation hook from react-router-dom to get the location object
   const location = useLocation();
   // Access the state object from the location
   const results = location.state?.results || [];
+  //const [filteredResults, setFilteredResults] = useState(results);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   console.log("Results to render:", results);
+
+
+  useEffect(() => {
+    const lowerCasedTerm = searchTerm.toLowerCase().trim();
+    console.log("Lowercased Term:", lowerCasedTerm);
+
+
+    const filtered = results.filter((result) =>
+    result.title.toLowerCase().trim().startsWith(lowerCasedTerm));
+
+    console.log("Filtered Result:", filtered);
+
+    setFilteredResults(filtered);
+
+    if (onSearch) {
+      onSearch(lowerCasedTerm, filtered);
+    }
+  }, [searchTerm, results, onSearch]);
+
 
   return (
     <div>
       {/* Render your search results here */}
-      {results.map((result) => (
+      {filteredResults.map((result) => (
         <div key={result._id}>
-          {result.source === "skill" && <SkillResultItem result={result} />}
-          {result.source === "class" && <ClassResultItem result={result} />}
+          {/*<Link to={`/details/${result._id}`}>*/}
+          {result.source === "skill" && result.title && <SkillResultItem result={result} />}
+          {result.source === "class" && result.title && <ClassResultItem result={result} />}
         </div>
       ))}
     </div>
