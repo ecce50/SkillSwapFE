e11fbee998f088a1afe824ddd55f002e7a81e96a
Peter R <prodger2@gmail.com>
2024-04-06 11:24:56 +0200

    fixed search result links

--- src/components/search/ClassResultItem.jsx
+++ src/components/search/ClassResultItem.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ClassResultItem = ({ result }) => {
   console.log("Rendering ClassResultItem with result:", result);
+  console.log("Class Result Item SkillId: ", result.skillId);
+  const idAsString = result._id.toString();
+  console.log("This is the id as a string: ", idAsString);
   return (
-    <div>
-      <h3 style={{ color: "blue" }}>{result.title}</h3>
+    <div id={idAsString}>
+      <Link to={`/skill-detail/${result.skillId}#${idAsString}`}>
+        <h2 style={{ color: "blue" }}>{result.title}</h2>
+      </Link>
       <p>{result.description}</p>
       {/* Add more Class-specific details or styling as needed */}
     </div>
--- src/components/search/SkillResultItem.jsx
+++ src/components/search/SkillResultItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const SkillResultItem = ({ result }) => {
   console.log("Rendering SkillResultItem with result:", result);
   return (
     <div>
-      <h2 style={{ color: "green" }}>{result.title}</h2>
+      <Link to={`/skill-detail/${result._id}`}>
+        <h2 style={{ color: "green" }}>{result.title}</h2>
+      </Link>
       <p>{result.description}</p>
-      {/* Add more Skill-specific details or styling as needed */}
     </div>
   );
 };
